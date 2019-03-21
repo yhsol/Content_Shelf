103166140665a8736d2ffcc6408828d7b369547f
yhsol <yhsol1592@gmail.con>
2019-03-21 18:55:52 +0900

    add API

--- src/Api.js
+++ src/Api.js
@@ -21,11 +21,7 @@ export const moviesApi = {
 	search: (term) =>
 		api.get('search/movie', {
 			params: {
-<<<<<<< HEAD
-				query: term
-=======
 				query: encodeURIComponent(term)
->>>>>>> bd8293317388039d34d73d0e015714b65be1da17
 			}
 		})
 };
@@ -43,11 +39,7 @@ export const tvApi = {
 	search: (term) =>
 		api.get('search/tv', {
 			params: {
-<<<<<<< HEAD
-				query: term
-=======
 				query: encodeURIComponent(term)
->>>>>>> bd8293317388039d34d73d0e015714b65be1da17
 			}
 		})
 };
